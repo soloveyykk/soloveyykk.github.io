211a0cebc1ebaaa5330b222bbe0f3b9ab5e3808b
soloveyykk <48865384+soloveyykk@users.noreply.github.com>
2019-03-23 21:30:03 +0200

    Add files via upload

---
+++ CS_GO/js/js.js
@@ -0,0 +1,16 @@
+$(function () {
+     $("#burg-menu").on("click", function (event) {
+         event.preventDefault();
+
+         $(this).toggleClass("active");
+         $("#nav").toggleClass("active");
+     });
+
+     function checkScroll(scrollOffset) {
+         if (scrollOffset >= introH) {
+             header.addClass("fixed");
+         } else {
+             header.removeClass("fixed");
+         }
+     }
+});
