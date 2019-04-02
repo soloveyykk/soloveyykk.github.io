cce5d1957d76c9aa7371b9de134c338b826c846a
soloveyykk <48865384+soloveyykk@users.noreply.github.com>
2019-04-02 23:01:43 +0300

    Add files via upload

---
+++ kursova/telegram-lesson/telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot850657526:AAFeKynOFsTijrAN-Fgoci240AjkUSb-i30/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "850657526:AAFeKynOFsTijrAN-Fgoci240AjkUSb-i30";
+$chat_id = "504777726";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
